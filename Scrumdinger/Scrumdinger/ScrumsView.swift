cbb30264a1e75828ccc105a5bf33c01916325582
김태성 <ts4683@gmail.com>
2022-05-01 22:24:25 +0900

    Views
    - Creating a Card View
    - Displaying Data in a List
    
    *Must copy Assets to load colors*

---
+++ Scrumdinger/Scrumdinger/ScrumsView.swift
@@ -0,0 +1,28 @@
+//
+//  ScrumsView.swift
+//  Scrumdinger
+//
+//  Created by 김태성 on 2022/05/01.
+//
+
+import SwiftUI
+
+struct ScrumsView: View {
+    let scrums: [DailyScrum]
+    
+    var body: some View {
+        List {
+            ForEach(scrums) { scrum in
+                CardView(scrum: scrum)
+                    .listRowBackground(scrum.theme.mainColor)
+            }
+        }
+    }
+}
+
+struct ScrumsView_Previews: PreviewProvider {
+    static var previews: some View {
+        ScrumsView(scrums: DailyScrum.sampleData)
+    }
+}
+
