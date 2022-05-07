29eb8725c3bfb6d14dcc95ab2e836474678c141f
moook <ts4683@gmail.com>
2022-05-08 01:02:29 +0900

    Section 8 Generate Previews Dynamically
    #preview device name MUST be same name on Xcode device

--- Landmarks/Landmarks/Views/LandmarkList.swift
+++ Landmarks/Landmarks/Views/LandmarkList.swift
@@ -24,6 +24,10 @@ struct LandmarkList: View {
 
 struct LandmarkList_Previews: PreviewProvider {
     static var previews: some View {
-        LandmarkList()
+        ForEach(["iPhone SE (3nd generation)", "iPhone 12 Pro", "iPad Pro (9.7-inch)"], id: \.self) { deviceName in
+            LandmarkList()
+                .previewDevice(PreviewDevice(rawValue: deviceName))
+                .previewDisplayName(deviceName)
+        }
     }
 }
