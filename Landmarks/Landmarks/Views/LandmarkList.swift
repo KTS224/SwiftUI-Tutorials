a73153f62636b5b881d3c772ca815f723377d3f3
moook <ts4683@gmail.com>
2022-05-08 00:08:46 +0900

    Section 4 Create the List of Landmarks /

---
+++ Landmarks/Landmarks/Views/LandmarkList.swift
@@ -0,0 +1,23 @@
+//
+//  LandmarkList.swift
+//  Landmarks
+//
+//  Created by 김태성 on 2022/05/08.
+//
+
+import SwiftUI
+
+struct LandmarkList: View {
+    var body: some View {
+        List {
+            LandmarkRow(landmark: landmarks[0])
+            LandmarkRow(landmark: landmarks[1])
+        }
+    }
+}
+
+struct LandmarkList_Previews: PreviewProvider {
+    static var previews: some View {
+        LandmarkList()
+    }
+}
